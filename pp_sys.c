cd04333443be433ce56e0d3a9eaa7956c730f62e
Nicolas R <atoomic@cpan.org>
2018-07-16 11:26:59 -0600

    skip shadow call when euid > 0 on linux
    maybe __linux__ is not restrictive enough
    and we could consider using a hint sh file
    to enable it only on some specific distro?
    
    Upstream-URL: https://rt.perl.org/Ticket/Display.html?id=133388

--- pp_sys.c
+++ pp_sys.c
@@ -5549,6 +5549,9 @@ PP(pp_gpwent)
         /* Some AIX setups falsely(?) detect some getspnam(), which
          * has a different API than the Solaris/IRIX one. */
 #   if defined(HAS_GETSPNAM) && !defined(_AIX)
+#      ifdef __linux__
+    if (!PerlProc_geteuid())
+#      endif
         {
             dSAVE_ERRNO;
             const struct spwd * const spwent = getspnam(pwent->pw_name);
